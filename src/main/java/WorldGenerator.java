aee2acc8f4a6eea1fc7ad7c081fbecdb14f20912
ZavGaro <zaharof2005@gmail.com>
2024-10-09 21:20:06 +0300

    Change generation slightly

--- src/main/java/WorldGenerator.java
+++ src/main/java/WorldGenerator.java
@@ -14,13 +14,13 @@ public class WorldGenerator {
     Chunk generateChunk(VectorI indexes) {
         System.out.printf("Generates chunk (%d;%d)\n", indexes.x, indexes.y);
         Chunk chunk = new Chunk(subworld);
+        chunk.yIndex = indexes.y;
         int baseX = Chunk.size() * indexes.x;
         int baseY = Chunk.size() * indexes.y;
         int i = 0;
         for (int y = 0; y < Chunk.size(); y++)
             for (int x = 0; x < Chunk.size(); x++) {
-                if (baseY + y + Math.sin((baseX + x)*0.5)*50 < 0) {
-//                if (Math.sqrt(Math.pow(baseX + x, 2) + Math.pow(baseY + y, 2)) < 40) {
+                if (baseY + y < Math.sin((baseX + x)*0.1)*0.5*Math.abs(x+baseX)) {
                     chunk.presetPixel(new Pixel(Pixels.getPixelWithRandomColor(1), null, baseX + x, baseY + y));
                 } else {
                     chunk.pixels[i] = (new Pixel(0, chunk, baseX + x, baseY + y));
