8dde15a5f188b68ab5450d639b72dd1b0eabda0c
Mechanik <Step03-orel@ya.ru>
2024-12-04 14:13:19 +0300

    Add Request class for messages

---
+++ src/main/java/game/NetMessage/Request.java
@@ -0,0 +1,27 @@
+package game.NetMessage;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class Request {
+    static byte id = Messages.getNextMessageIndex();
+    byte requestedID; //ID of the requested message
+    int requestData = 0; // information with request to server(optional)
+
+    public Request(byte requestedID) {
+        this.requestedID = requestedID;
+    }
+
+    public Request(byte requestedID,int requestData) {
+        this.requestedID = requestedID;
+        this.requestData = requestData;
+    }
+
+    public byte[] buildMessage() {
+            ByteBuffer message = ByteBuffer.allocate(2+Integer.BYTES);
+            message.put(id);
+            message.put(requestedID);
+            message.putInt(requestData);
+            return message.array();
+        }
+}
