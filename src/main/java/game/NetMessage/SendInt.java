41c55519e80b1401bb4793bc5f9de28d3ec4953f
Mechanik <Step03-orel@ya.ru>
2024-12-05 15:48:20 +0300

    add SendInt class

--- src/main/java/game/NetMessage/SendInt.java
+++ src/main/java/game/NetMessage/SendInt.java
@@ -1,4 +1,15 @@
 package game.NetMessage;
 
-public class SendInt {
+import java.nio.ByteBuffer;
+
+public class SendInt extends Message{
+    static byte id = Messages.getNextMessageIndex();
+    static int intToSend;
+
+    public byte[] buildMessage() {
+        ByteBuffer message = ByteBuffer.allocate(1+Integer.BYTES);
+        message.put(id);
+        message.putInt(intToSend);
+        return message.array();
+    }
 }
